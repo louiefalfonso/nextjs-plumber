25c32d58474d7d52afaa1fedfb44c6c159796f6f
Louie Alfonso <lfriasalfonso@gmail.com>
2023-11-08 22:24:05 +0000

    nextjs plumber project
    nextjs plumber project

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import AboutArea from '@/components/AboutArea'
 import HeroAreas from '../components/HeroAreas'
-import ScrollToTop from '../components/ScrollToTop'
 import ServiceArea from '@/components/ServiceArea'
 import ContactArea from '@/components/ContactArea'
 import FeatureArea from '@/components/FeatureArea'
