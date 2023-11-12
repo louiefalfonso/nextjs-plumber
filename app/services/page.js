bd0241b3c031c9f8c1472622c0af1bbab82cc547
Louie Alfonso <lfriasalfonso@gmail.com>
2023-11-12 17:34:15 +0000

    nextjs plumber project
    nextjs plumber project

--- app/services/page.js
+++ app/services/page.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Link from 'next/link'
+import ServiceLists from '@/components/ServiceLists'
+import CtaArea from '@/components/CtaArea'
 
 export default function page() {
   return (
     <>
-    hello from services pages
+    <div className="breadcumb-wrapper " data-bg-src="img/bg/breadcumb-bg.jpg">
+        <div className="container">
+            <div className="breadcumb-content">
+                <h1 className="breadcumb-title">Services</h1>
+                <ul className="breadcumb-menu">
+                    <li><Link href="/" passHref>Home</Link></li>
+                    <li>Services</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <ServiceLists/>
+    <CtaArea/>
     </>
   )
 }
---
+++ components/ServiceLists.js
@@ -0,0 +1,200 @@
+import React from 'react'
+
+export default function ServiceLists() {
+  return (
+    <>
+    <section className="overflow-hidden space">
+        <div className="container">
+            <div className="row gy-30">
+                <div className="col-md-6 col-xl-4">
+                    <div className="service-block">
+                    <div className="shape1" />
+                    <div className="service-block_icon">
+                        <div className="icon">
+                        <img src="img/icon/service_block_1.svg" alt="Icon" />
+                        </div>
+                    </div>
+                    <div className="service-block_img">
+                        <img src="img/service/service_block_1.jpg" alt="Image" />
+                    </div>
+                    <h3 className="box-title">
+                        <a href="service-details.html">Basement Plumbing</a>
+                    </h3>
+                    <p className="service-block_text">
+                        Basement plumbing is an essential part of any home. You are looking
+                        to finish your basement or just need repairs.
+                    </p>
+                    <a href="service-details.html" className="themeholy-btn">
+                        Read More
+                    </a>
+                    <div className="ripple-shape">
+                        <span className="ripple-1" />
+                        <span className="ripple-2" />
+                        <span className="ripple-3" />
+                        <span className="ripple-4" />
+                        <span className="ripple-5" />
+                        <span className="ripple-6" />
+                    </div>
+                    </div>
+                </div>
+                <div className="col-md-6 col-xl-4">
+                    <div className="service-block">
+                    <div className="shape1" />
+                    <div className="service-block_icon">
+                        <div className="icon">
+                        <img src="img/icon/service_block_2.svg" alt="Icon" />
+                        </div>
+                    </div>
+                    <div className="service-block_img">
+                        <img src="img/service/service_block_2.jpg" alt="Image" />
+                    </div>
+                    <h3 className="box-title">
+                        <a href="service-details.html">Drain Plumbing</a>
+                    </h3>
+                    <p className="service-block_text">
+                        Drain plumbing is a critical component of any homes plumbing system.
+                        When drains become clogged or damaged.
+                    </p>
+                    <a href="service-details.html" className="themeholy-btn">
+                        Read More
+                    </a>
+                    <div className="ripple-shape">
+                        <span className="ripple-1" />
+                        <span className="ripple-2" />
+                        <span className="ripple-3" />
+                        <span className="ripple-4" />
+                        <span className="ripple-5" />
+                        <span className="ripple-6" />
+                    </div>
+                    </div>
+                </div>
+                <div className="col-md-6 col-xl-4">
+                    <div className="service-block">
+                    <div className="shape1" />
+                    <div className="service-block_icon">
+                        <div className="icon">
+                        <img src="img/icon/service_block_3.svg" alt="Icon" />
+                        </div>
+                    </div>
+                    <div className="service-block_img">
+                        <img src="img/service/service_block_3.jpg" alt="Image" />
+                    </div>
+                    <h3 className="box-title">
+                        <a href="service-details.html">Water Line Repair</a>
+                    </h3>
+                    <p className="service-block_text">
+                        A broken or leaking water line can cause significant damage to your
+                        home and property. If you suspect a problem.
+                    </p>
+                    <a href="service-details.html" className="themeholy-btn">
+                        Read More
+                    </a>
+                    <div className="ripple-shape">
+                        <span className="ripple-1" />
+                        <span className="ripple-2" />
+                        <span className="ripple-3" />
+                        <span className="ripple-4" />
+                        <span className="ripple-5" />
+                        <span className="ripple-6" />
+                    </div>
+                    </div>
+                </div>
+                <div className="col-md-6 col-xl-4">
+                    <div className="service-block">
+                    <div className="shape1" />
+                    <div className="service-block_icon">
+                        <div className="icon">
+                        <img src="img/icon/service_block_4.svg" alt="Icon" />
+                        </div>
+                    </div>
+                    <div className="service-block_img">
+                        <img src="img/service/service_block_4.jpg" alt="Image" />
+                    </div>
+                    <h3 className="box-title">
+                        <a href="service-details.html">Kitchen Plumbing</a>
+                    </h3>
+                    <p className="service-block_text">
+                        Some common kitchen plumbing issues include clogged drains, leaky
+                        faucets, malfunctioning garbage disposals.
+                    </p>
+                    <a href="service-details.html" className="themeholy-btn">
+                        Read More
+                    </a>
+                    <div className="ripple-shape">
+                        <span className="ripple-1" />
+                        <span className="ripple-2" />
+                        <span className="ripple-3" />
+                        <span className="ripple-4" />
+                        <span className="ripple-5" />
+                        <span className="ripple-6" />
+                    </div>
+                    </div>
+                </div>
+                <div className="col-md-6 col-xl-4">
+                    <div className="service-block">
+                    <div className="shape1" />
+                    <div className="service-block_icon">
+                        <div className="icon">
+                        <img src="img/icon/service_block_5.svg" alt="Icon" />
+                        </div>
+                    </div>
+                    <div className="service-block_img">
+                        <img src="img/service/service_block_5.jpg" alt="Image" />
+                    </div>
+                    <h3 className="box-title">
+                        <a href="service-details.html">Gas Line Services</a>
+                    </h3>
+                    <p className="service-block_text">
+                        Gas lines are used to supply natural gas or propane to appliances
+                        such as stoves, ovens, water heaters, and furnaces..
+                    </p>
+                    <a href="service-details.html" className="themeholy-btn">
+                        Read More
+                    </a>
+                    <div className="ripple-shape">
+                        <span className="ripple-1" />
+                        <span className="ripple-2" />
+                        <span className="ripple-3" />
+                        <span className="ripple-4" />
+                        <span className="ripple-5" />
+                        <span className="ripple-6" />
+                    </div>
+                    </div>
+                </div>
+                <div className="col-md-6 col-xl-4">
+                    <div className="service-block">
+                    <div className="shape1" />
+                    <div className="service-block_icon">
+                        <div className="icon">
+                        <img src="img/icon/service_block_6.svg" alt="Icon" />
+                        </div>
+                    </div>
+                    <div className="service-block_img">
+                        <img src="img/service/service_block_6.jpg" alt="Image" />
+                    </div>
+                    <h3 className="box-title">
+                        <a href="service-details.html">Bathroom Services</a>
+                    </h3>
+                    <p className="service-block_text">
+                        Bathroom plumbing is a critical part of any homes plumbing system.
+                        From sinks and toilets to showers and bathtubs.
+                    </p>
+                    <a href="service-details.html" className="themeholy-btn">
+                        Read More
+                    </a>
+                    <div className="ripple-shape">
+                        <span className="ripple-1" />
+                        <span className="ripple-2" />
+                        <span className="ripple-3" />
+                        <span className="ripple-4" />
+                        <span className="ripple-5" />
+                        <span className="ripple-6" />
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    </>
+  )
+}
