7ceb61d4634f57a746f0a268426cb2cb650c753d
Louie Alfonso <lfriasalfonso@gmail.com>
2023-11-13 15:12:44 +0000

    nextjs plumber project
    nextjs plumber project

--- app/layout.js
+++ app/layout.js
@@ -18,13 +18,12 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet"/>
-        <link rel="stylesheet" href="../css/bootstrap.min.css"/>
-        <link rel="stylesheet" href="../css/fontawesome.min.css"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700&display=swap"/>
+        <link rel="preconnect" href="../css/bootstrap.min.css"/>
+        <link rel="preconnect" href="../css/fontawesome.min.css"/>
         <link rel="stylesheet" href="../css/magnific-popup.min.css"/>
         <link rel="stylesheet" href="../css/slick.min.css"/>
-        <link rel="stylesheet" href="../css/style.css" as="style" onload="this.onload=null;this.rel='stylesheet'"
-        />
+        <link rel="stylesheet" href="../css/style.css"/>
       </head>
       
       <body>
